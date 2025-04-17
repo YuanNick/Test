dc0749f5139d33fa61e9c2b7067468e025e8e3a7
陳君豪(Howard Chen) <howard.chen@iisigroup.com>
2025-04-17 16:19:09 +0800

    增加http錯誤攔截器, properties設定log

--- src/main/java/com/sample/resource/ResourceSecurityConfig.java
+++ src/main/java/com/sample/resource/ResourceSecurityConfig.java
@@ -1,13 +1,27 @@
 package com.sample.resource;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -15,6 +29,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ResourceSecurityConfig {
 
     private final PermissionManager permissionManager;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     @Bean
     @Order(2)
@@ -32,6 +47,7 @@ public class ResourceSecurityConfig {
                                     jwt.jwkSetUri("http://localhost:9000/oauth2/jwks");
                                 }
                         )
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
                 );
 
         return http.build();
